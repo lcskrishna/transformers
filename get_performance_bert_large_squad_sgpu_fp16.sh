d4d4a856297b615a687928e0f9383de5325bafee
Chaitanya Sri Krishna Lolla <lollachaitanya@gmail.com>
2020-03-18 10:48:42 -0700

    Add fp16 script to run.

---
+++ get_performance_bert_large_squad_sgpu_fp16.sh
@@ -0,0 +1,20 @@
+export SQUAD_DIR=/data/datasets/wikipedia_bookcorpus_hdf5/download/squad/v1.1/
+
+export CUDA_VISIBLE_DEVICES=0
+
+python examples/run_squad.py \
+  --model_type bert \
+  --model_name_or_path bert-large-uncased-whole-word-masking \
+  --do_train \
+  --do_lower_case \
+  --train_file $SQUAD_DIR/train-v1.1.json \
+  --predict_file $SQUAD_DIR/dev-v1.1.json \
+  --per_gpu_train_batch_size 4 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 1.0 \
+  --max_seq_length 512 \
+  --doc_stride 128 \
+  --output_dir perf \
+  --max_steps 1000 \
+  --overwrite_output_dir \
+  --fp16
